ea9ee123cd757fcada889890868510c5ad6c3d0b
fanpei91 <fanpei91@gmail.com>
2020-05-04 11:56:33 +0800

    cross for other platform

---
+++ sysproxy_others.go
@@ -0,0 +1,15 @@
+// +build !darwin
+
+package main
+
+func setSysProxy(_ string) error {
+	return nil
+}
+
+func unsetSysProxy() error {
+	return nil
+}
+
+func getNetworkInterface() string {
+	return ""
+}
